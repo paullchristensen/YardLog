90eec5ab90aac3c814e219392a9a57569593ec84
Paul Christensen <Paul.Christensen@rae.com>
2020-01-28 14:49:10 -0700

    "Landing Page Added"

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-//THIS IS A TEST
+var express    = require("express");
+var bodyParser = require("body-parser");
 
-//This is 2nd Test
+var app     = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs"); //so that .ejs is not needed in render page
+app.use(express.static("public"));
+
+
+
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("YardLog Server started!!!");
+});
